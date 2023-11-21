8bc6d770f641e092d9f5d50f35fcd4aa014a0b34
atheek2003 <atheekhebbar@gmail.com>
2023-11-21 12:14:05 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT ||5000
 const cors = require('cors')
-const path = require('path')
 //middleware makes connection to frontend side
 
 app.use(cors());
 app.use(express.json());
-
-const parentDir = path.resolve(__dirname, '..');
  
-app.get('*', (req, res) => {
-  res.sendFile(path.join(parentDir, 'frontend','index.html'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
+
 //mongodb 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
